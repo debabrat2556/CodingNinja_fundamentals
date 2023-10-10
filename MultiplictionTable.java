e3a131b78c9dfc51fa637fdf25c855c2ff6a3b1e
debabrat2556 <debabrat863827@gmail.com>
2023-10-10 23:47:32 +0530

    Multiplication Table

---
+++ MultiplictionTable.java
@@ -0,0 +1,21 @@
+package CodingNinja_fundamentals;
+
+import java.util.Scanner;
+
+public class MultiplictionTable {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter a value");
+        
+        //Please delete the above line
+
+        int digit=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            int result=digit*i;
+            System.out.print(digit+" x "+i);
+            System.out.print(" = "+result);
+            System.out.println();
+        }
+    }
+}

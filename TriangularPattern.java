db6af329cadfeababa2d4b2c96bfe39bf097611c
debabrat2556 <debabrat863827@gmail.com>
2023-10-12 23:14:47 +0530

    Triangular Pattern

---
+++ TriangularPattern.java
@@ -0,0 +1,28 @@
+// Triangular Star Pattern
+// Send Feedback
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
+// *
+// **
+// ***
+// ****
+
+package CodingNinja_fundamentals;
+
+import java.util.Scanner;
+
+public class TriangularPattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter number of rows");
+        int rows=sc.nextInt(); 
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

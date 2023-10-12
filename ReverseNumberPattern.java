4f16e4bf6282b77bc6243462c61446a19b936e3b
debabrat2556 <debabrat863827@gmail.com>
2023-10-12 23:54:23 +0530

    Reverse Number Pattern added

---
+++ ReverseNumberPattern.java
@@ -0,0 +1,18 @@
+package CodingNinja_fundamentals;
+
+import java.util.Scanner;
+
+public class ReverseNumberPattern {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int rows=sc.nextInt();
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=i;j>=1;j--)
+        {
+            System.out.print(j+"");
+        }
+        System.out.println();
+    }
+    }
+}

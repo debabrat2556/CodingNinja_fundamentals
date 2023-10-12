d392d4dae51e0a88d8dcf95934c5d96cf76405c2
debabrat2556 <debabrat863827@gmail.com>
2023-10-12 23:29:59 +0530

    Triangular Number Pattern

---
+++ TriangleNumberPattern.java
@@ -0,0 +1,27 @@
+// Triangle Number Pattern
+// Send Feedback
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
+// 1
+// 22
+// 333
+// 4444
+
+package CodingNinja_fundamentals;
+
+import java.util.Scanner;
+
+public class TriangleNumberPattern {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int rows=sc.nextInt();
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(i+"");
+        }
+        System.out.println();
+    }
+}    
+}

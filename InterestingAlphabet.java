3fcf3735546f6c5a5be96d8ac1e027018cdc7558
Debabrat Adhikary <145679102+debabrat2556@users.noreply.github.com>
2023-10-15 13:58:08 +0530

    Explained InterestingAlphabet.java

--- InterestingAlphabet.java
+++ InterestingAlphabet.java
@@ -14,19 +14,20 @@ public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     System.out.println("Please enter row number");
     int rowCount=sc.nextInt();
-    char InterestingAlpha=(char)((int)'A'+rowCount-1); 
+    char InterestingAlpha=(char)((int)'A'+rowCount-1); //charcter value is converted to E now
     for(int i=1;i<=rowCount;i++)
     {
-        char InterestingAlpha2=InterestingAlpha;
+        char InterestingAlpha2=InterestingAlpha; // storing value of Interesting Alpha to a different character variable
         for(int j=0;j<i;j++)
         {
-            System.out.print(InterestingAlpha2);
-            InterestingAlpha2=(char)((int)InterestingAlpha2+1);
+            System.out.print(InterestingAlpha2); //printing what's in InterestingAlpha2
+            InterestingAlpha2=(char)((int)InterestingAlpha2+1); //updating InterestingAlpha2 by 1 so if InterestingAlpha2 is E now the value is F
         }
         
-        InterestingAlpha=(char)((int)InterestingAlpha-1);
+        InterestingAlpha=(char)((int)InterestingAlpha-1); //If we properly check InterestingAlpha still has the value 'E' so 'E'-1=D
+                                                          //So for the next round the value passed is under InterestingAlpha is 'D'
         
-        System.out.println();
+        System.out.println();    //adding line space
     }
 }    
 }

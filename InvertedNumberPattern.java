3ad4eaa49184754d40fbac298dd4f65da6d81d68
Debabrat Adhikary <145679102+debabrat2556@users.noreply.github.com>
2023-10-15 16:15:52 +0530

    Explained InvertedNumberPattern.java

--- InvertedNumberPattern.java
+++ InvertedNumberPattern.java
@@ -15,12 +15,14 @@ public static void main(String[] args) {
         System.out.println("please enter number of rows");
         int rowCount=sc.nextInt();
         int number=rowCount;
-        for(int i=1;i<=rowCount;i++) 
+        for(int i=1;i<=rowCount;i++) // this loop will run for all the rows, 
         {
 
-            for(int j=rowCount;j>=i;j--)
+            for(int j=rowCount;j>=i;j--) //this loop will run for repetation, for first itereation
+                                          // j=rowCount i.e j=4(say) and i value for first iteration is 1 so this loop will run till j=i or j>i
             {
-                System.out.print(number);
+                System.out.print(number);        // what needs to be printed (for first iteration only 4 is reqiored) 
+                                                 // So 4 needs to be printed a totatl of 4 times
             }
                 
                 number=number-1;

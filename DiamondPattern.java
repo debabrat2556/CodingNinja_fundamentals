6b0c85fe1af0aadb5ff066a610236730af0fec4e
debabrat2556 <debabrat863827@gmail.com>
2023-10-17 00:28:37 +0530

    Comment added for Diamond Pattern problem please go through

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -26,6 +26,8 @@ public class DiamondPattern {
         int firstHalf=rowCount/2+1;
         int secondHalf=firstHalf-1;
 
+        //This is the first half of diamond
+
         for(int i=0;i<firstHalf;i++) //outer loop to run the rows
         {
             for(int space=firstHalf-1;space>i;space--) //firsthalf value is say 3 so space=2 and i is 0
@@ -40,13 +42,33 @@ public class DiamondPattern {
             }
             System.out.print(" ");                      // we have added this line just to showcase 
                                                             //how the program works
-                                                            
+
             for(int secondStar=0;secondStar<i;secondStar++) //i is at 0 for the first instance
                                                             // and we are also initializing secondStar at 0
                                                             // so for first row no output
                                                             //but for second iteration seconStar=0
                                                                                 // and i=1
                                                                                 //secondStar<i so condition satisfies
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();               //added this line to discriminate between two halves
+        
+        // The bottom lines of code is for the second half of the diamond
+        for(int i=1;i<=secondHalf;i++) //the outer layer for rows to move
+        {
+            for(int space=1;space<=i;space++)   //we require equal number of space as of row count
+            {
+                System.out.print(" ");
+            }            
+            for(int star1=secondHalf;star1>i;star1--)
+            {
+                System.out.print("*");
+            }
+            System.out.print(" ");
+            for(int star2=secondHalf;star2>=i;star2--)
             {
                 System.out.print("*");
             }

8cae2bda0ab39187247aa6df41e7889f3b81662a
debabrat2556 <debabrat863827@gmail.com>
2023-10-16 23:03:49 +0530

    star pattern and triangular number pattern has been added

---
+++ StarPattern.java
@@ -0,0 +1,38 @@
+// Star Pattern
+// Send Feedback
+// Print the following pattern
+// Pattern for N = 4
+// ...*              ...*
+// ..***     =       ..**        +       *
+// .*****            .***                **
+// *******           ****                ***
+
+//understand the above pattern first (******THIS IS THE HINT*****)
+
+// Hint
+// As taught in the video, you just have to modify the code so that instead of printing numbers, it should output stars ('*').
+
+import java.util.Scanner;
+public class StarPattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter row number");
+        int rowCount=sc.nextInt();
+        for(int i=0;i<rowCount;i++)
+        {
+            for(int space=rowCount-1;space>i;space--)
+            {
+                System.out.print(" ");
+            }
+            for(int starRepetation=0;starRepetation<=i;starRepetation++)
+            {
+                System.out.print("*");
+            }
+            for(int secondStar=0;secondStar<i;secondStar++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ TriangleOfNumbers.java
@@ -0,0 +1,43 @@
+// Code : Triangle of Numbers
+// Send Feedback
+// Print the following pattern for the given number of rows.
+// Pattern for N = 4
+// ...1
+// ..232
+// .34543
+// 4567654
+
+
+// The dots represent 
+
+//For Hint check StarPattern.java
+
+import java.util.Scanner;
+
+public class TriangleOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter row number");
+        int rowCount=sc.nextInt();
+        for(int i=1;i<=rowCount;i++)
+        {
+            for(int space=rowCount;space>i;space--)
+            {
+                System.out.print(" "+" ");
+            }
+            int number1=i;
+            for(int starRepetation=0;starRepetation<i;starRepetation++)
+            {
+                System.out.print(number1+" ");
+                number1++;
+            }
+            int number2=number1-2;
+            for(int secondStar=1;secondStar<i;secondStar++)
+            {
+                System.out.print(number2+" ");
+                number2--;
+            }
+            System.out.println();
+        }
+    }
+}
